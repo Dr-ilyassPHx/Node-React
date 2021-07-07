838f0dd4f5c2b54c14848a52a0c442cc89d42ffb
AAGH <abdellah-aghlalou@outlook.fr>
2021-07-07 21:24:56 +0100

    test first commiting backend branch

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routesHandler = require('./routes/handler.js');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -10,5 +11,6 @@ app.use('/', routesHandler);
 
 const PORT = 5000; // backend routing port
 app.listen(PORT, () => {
-    console.log(`Server ... on port ${PORT}`);
+    console.log("server on port" ${port});
+    console.log("test test ");
 })
